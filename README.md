2a60532e7a981ef2b704b69516f45b6d6aa66ce8
Matthew Kayne <matthewkayne2004@gmail.com>
2022-03-26 16:16:47 +0000

    pylint

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Sudoku-Solver
+<h1 align="center">Sudoku-Solver</h1>
 
-[![CodeFactor](https://www.codefactor.io/repository/github/matthewkayne/sudoku-solver/badge)](https://www.codefactor.io/repository/github/matthewkayne/sudoku-solver)
+<p align="center"><a href="https://www.codefactor.io/repository/github/matthewkayne/sudoku-solver"><img src="https://www.codefactor.io/repository/github/matthewkayne/sudoku-solver/badge" alt="CodeFactor" /></a></p>
 
 Solves any 9x9 sudoku board
--- solve.py
+++ solve.py
@@ -1,20 +1,24 @@
+"""Sudoku Solver"""
 # Define Sudoku board
-board = [[6,4,0,0,0,0,0,0,2],
-         [9,0,0,0,5,0,7,0,0],
-         [0,8,0,0,0,3,0,0,0],
-         [0,0,3,0,6,0,1,0,0],
-         [0,0,0,3,0,8,0,0,0],
-         [0,0,4,0,7,0,5,0,0],
-         [0,0,0,8,0,0,0,7,0],
-         [0,0,7,0,2,0,0,0,9],
-         [5,0,0,0,0,0,0,6,4]]
+board = [[6, 4, 0, 0, 0, 0, 0, 0, 2],
+         [9, 0, 0, 0, 5, 0, 7, 0, 0],
+         [0, 8, 0, 0, 0, 3, 0, 0, 0],
+         [0, 0, 3, 0, 6, 0, 1, 0, 0],
+         [0, 0, 0, 3, 0, 8, 0, 0, 0],
+         [0, 0, 4, 0, 7, 0, 5, 0, 0],
+         [0, 0, 0, 8, 0, 0, 0, 7, 0],
+         [0, 0, 7, 0, 2, 0, 0, 0, 9],
+         [5, 0, 0, 0, 0, 0, 0, 6, 4]]
 
 # Checks if a number fits the constraints
-def possible(y,x,n):
+
+
+def possible(y, x, n):
+    """Possible"""
     for i in range(9):
         if board[y][i] == n:
             return False
-    for j in range(9):      
+    for j in range(9):
         if board[j][x] == n:
             return False
     xbox = (x//3)*3
@@ -26,18 +30,26 @@ def possible(y,x,n):
     return True
 
 # Solves the board
+
+
 def solve():
+    """Solve"""
     for y in range(9):
         for x in range(9):
             if board[y][x] == 0:
-                for n in range(1,10):
-                    if possible(y,x,n):
+                for n in range(1, 10):
+                    if possible(y, x, n):
                         board[y][x] = n
                         # Recursion
                         solve()
                         # Backtracking
                         board[y][x] = 0
                 return None
+
     for i in range(9):
-        print(board[i])         
-solve()    
+        print(board[i])
+
+    return None
+
+
+solve()
